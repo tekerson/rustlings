cdca7f3a720c90fa76c53dd676ddff723d2bf193
Brenton Alker <brenton@tekerson.com>
2020-08-23 18:39:05 +0100

    Complete generics/generics3.rs

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 // An imaginary magical school has a new report card generation system written in Rust!
 // Currently the system only supports creating report cards where the student's grade
 // is represented numerically (e.g. 1.0 -> 5.5).
@@ -7,10 +9,23 @@
 // Make the necessary code changes to support alphabetical report cards, thereby making
 // the second test pass.
 
-// I AM NOT DONE
+pub enum Grade {
+    Numeric(f32),
+    Alphabetical(String),
+}
+
+impl Display for Grade {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        let s: String = match self {
+            Grade::Numeric(n) => n.to_string(),
+            Grade::Alphabetical(g) => g.to_string(),
+        };
+        formatter.write_str(&s)
+    }
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -29,8 +44,8 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Tom Wriggle".to_string(),
+            grade: Grade::Numeric(2.1), 
+            student_name: "Tom Wriggle".to_string(), 
             student_age: 12,
         };
         assert_eq!(
@@ -43,8 +58,8 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
+            grade: Grade::Alphabetical("A+".to_string()), 
+            student_name: "Gary Plotter".to_string(), 
             student_age: 11,
         };
         assert_eq!(
