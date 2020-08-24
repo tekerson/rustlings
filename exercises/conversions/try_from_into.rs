5677f9f187dada7b2fb5d29354b2b49f8d9e5189
Brenton Alker <brenton@tekerson.com>
2020-08-24 21:52:45 +0100

    Complete conversions/try_from_into.rs

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -11,8 +11,6 @@ struct Color {
     blue: u8,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need create implementation for a tuple of three integer,
@@ -26,6 +24,13 @@ struct Color {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = String;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b) = tuple;
+         
+        Ok(Color {
+            red: u8::try_from(r).map_err(|_| "Red Fail")?,
+            green: u8::try_from(g).map_err(|_| "Green Fail")?,
+            blue: u8::try_from(b).map_err(|_| "Blue Fail")?,
+        })
     }
 }
 
@@ -33,6 +38,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = String;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Color::try_from((arr[0], arr[1], arr[2]))
     }
 }
 
@@ -40,6 +46,13 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = String;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() < 3 {
+            Err("Not enough values".to_string())
+        } else if slice.len() > 3 {
+            Err("Too many values".to_string())
+        } else {
+            Color::try_from((slice[0], slice[1], slice[2]))
+        }
     }
 }
 
